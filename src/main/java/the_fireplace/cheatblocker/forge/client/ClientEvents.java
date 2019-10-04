950fdae4e37b241ebbc9a371c5a828b58c055245
The_Fireplace <the.f1repl4ce@gmail.com>
2019-10-04 17:43:42 -0500

    Fix copy paste issues (2)

--- src/main/java/the_fireplace/cheatblocker/forge/client/ClientEvents.java
+++ src/main/java/the_fireplace/cheatblocker/forge/client/ClientEvents.java
@@ -6,6 +6,7 @@ import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import the_fireplace.cheatblocker.CheatBlocker;
 
 @Mod.EventBusSubscriber(value = Side.CLIENT, modid = CheatBlocker.MODID)
 public class ClientEvents {
